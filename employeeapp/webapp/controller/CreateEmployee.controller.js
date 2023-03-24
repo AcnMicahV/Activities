d31323957c9839d7d63afda8ac5f95185e7b739f
Micah Vergara <micah.o.vergara@accenture.com>
2023-03-24 09:52:23 +0000

    commit 3/24/2023

--- employeeapp/webapp/controller/CreateEmployee.controller.js
+++ employeeapp/webapp/controller/CreateEmployee.controller.js
@@ -1,49 +1,159 @@
 sap.ui.define([
         "sap/ui/core/mvc/Controller",
-        "sap/ui/model/json/JSONModel",
-        "sap/m/MessageToast"
+        "sap/m/MessageToast",
+        "sap/ui/core/routing/History",
+        "sap/ui/core/ValueState",
+        "sap/m/MessageBox"
     ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel, MessageToast) {
+    function (Controller, MessageToast, History, ValueState, MessageBox) {
         "use strict";
 
         return Controller.extend("sapips.training.employeeapp.controller.CreateEmployee", {
             onInit: function () {
-
+                var oView = this.getView();
+                this.fnCheckValidation(oView);
             },
 
             onSave: function () {
                 var oModel = this.getOwnerComponent().getModel("EmployeesList");
 
                 var oView = this.getView();
-                var sEID = oView.byId("IEID").getValue();
+
                 var sFName = oView.byId("IFName").getValue();
                 var sLName = oView.byId("ILName").getValue();
                 var sAge = oView.byId("IAge").getValue();
-                var sDHire = oView.byId("IDHire").getValue();
+                var sDHire = new Date(oView.byId("IDHire").getValue());
                 var sCLevel = oView.byId("ICLevel").getSelectedKey();
                 var sCProj = oView.byId("ICProj").getSelectedKey();
 
+                var sEID = this.getEmployeeID(sFName, sLName).trim();
+
                 var oData = {
-                    "EmployeeID" : sEID,
-                    "FirstName" : sFName,
-                    "LastName" : sLName,
-                    "Age" : sAge,
-                    "DateHire" : sDHire,
-                    "CareerLevel" : sCLevel,
-                    "CurrentProject" : sCProj
+                    "EmployeeID": sEID,
+                    "FirstName": sFName,
+                    "LastName": sLName,
+                    "Age": sAge,
+                    "DateHire": sDHire,
+                    "CareerLevel": sCLevel,
+                    "CurrentProject": sCProj
 
                 }
 
-                oModel.create("/Employee", oData),
+                oModel.create("/Employee", oData, {
+                    success: function () {
+                        MessageToast.show("Employee has been created.");
+                    },
+
+                    error: function () {
+                        MessageToast.show("Failed to create employee.");
+                    }
+                });
 
                 this.getRouter().navTo("RouteEmployeeList");
             },
 
+            onNavBack: function () {
+                var oHistory = History.getInstance();
+                var sPreviousHash = oHistory.getPreviousHash();
+
+                if (sPreviousHash && sPreviousHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    this.getRouter().navTo("RouteEmployeeList");
+                }
+            },
+
+            onInputChange: function (oEvent) {
+                var oModel = this.getOwnerComponent().getModel("EmployeesList");
+
+                var oInput = oEvent.getSource(),
+                    sText = oInput.getValue();
+
+                if (sText.length === 0) {
+                    oInput.setValueState(ValueState.Error);
+                } else {
+                    oInput.setValueState(ValueState.None);
+                }
+
+                if (oInput.getId().includes('IAge')) {
+                    var age = parseInt(oInput.getValue());
+                    if (age < 0 || age > 90) {
+                        oInput.setValue("");
+                        oInput.setValueState(ValueState.Error);
+                        MessageBox.warning("Age cannot be less than 0 and greater than 90.");
+                    }
+                }
+
+                if (oInput.getId().includes('IDHire')) {
+                    var dateToday = new Date();
+                    if (new Date(oInput.getValue()) > dateToday) {
+                        oInput.setValue("");
+                        oInput.setValueState(ValueState.Error);
+                        MessageBox.warning("You cannot input future dates.");
+                    }
+                }
+
+                if (oInput.getId().includes('ICLevel')) {
+                    oModel.read("/CareerList(CareerID='" + oInput.getSelectedKey() + "')", {
+                        success: function (oCareer) {},
+
+                        error: function () {
+                            oInput.setValue("");
+                            oInput.setValueState(ValueState.Error);
+                            MessageBox.warning("Valid entries from the list only.");
+                        }
+                    });
+                }
+
+                if (oInput.getId().includes('CurrentProjectSelect')) {
+                    oModel.read("/ProjectList(ProjectID='" + oInput.getSelectedKey() + "')", {
+                        success: function (oCareer) {},
+
+                        error: function () {
+                            oInput.setValue("");
+                            oInput.setValueState(ValueState.Error);
+                            MessageBox.warning("Valid entries from the list only.");
+                        }
+                    });
+                }
+            },
+
             onCancel: function () {
                 this.getRouter().navTo("RouteEmployeeList");
+            },
+
+            fnCheckValidation: function (oView) {
+                var oFirstName = oView.byId("IFName");
+                oFirstName.attachBrowserEvent("keypress", function (event) {
+                    var inputValue = event.which;
+                    if (!(inputValue >= 65 && inputValue <= 120) && (inputValue != 32 && inputValue != 0)) {
+                        event.preventDefault();
+                    }
+                });
+
+                var oLastName = oView.byId("ILName");
+                oLastName.attachBrowserEvent("keypress", function (event) {
+                    var inputValue = event.which;
+                    if (!(inputValue >= 65 && inputValue <= 120) && (inputValue != 32 && inputValue != 0)) {
+                        event.preventDefault();
+                    }
+                });
+
+                var oAge = oView.byId("IAge");
+                oAge.attachBrowserEvent("keypress", function (event) {
+                    var keyCodes = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 0, 8];
+                    if (!($.inArray(event.which, keyCodes) >= 0)) {
+                        event.preventDefault();
+                    }
+                });
+            },
+
+            getEmployeeID: function (firstName, lastName) {
+                var dateToday = new Date();
+                return "EMPID" + firstName.toUpperCase() + lastName.toUpperCase() + dateToday.getDate() + dateToday.getMonth() + 1;
             },
 
             getRouter: function () {
--- employeeapp/webapp/controller/EmployeeList.controller.js
+++ employeeapp/webapp/controller/EmployeeList.controller.js
@@ -1,11 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageBox) {
+    function (Controller, Filter, FilterOperator, MessageBox, MessageToast) {
         "use strict";
 
         return Controller.extend("sapips.training.employeeapp.controller.EmployeeList", {
@@ -13,25 +16,31 @@ sap.ui.define([
                 
             },
 
+            onSearch: function (oEvent) {
+                var aFilters = [];
+                var sQuery = oEvent.getSource().getValue();
+                if (sQuery && sQuery.length > 0) {
+                    var filter = new Filter({
+                        filters: [
+                            new Filter("EmployeeID", FilterOperator.Contains, sQuery),
+                            new Filter("FirstName", FilterOperator.Contains, sQuery),
+                            new Filter("LastName", FilterOperator.Contains, sQuery),
+                        ],
+                        and: false,
+                    });
+                    aFilters.push(filter);
+                }
+    
+                var oList = this.byId("idEmployeesTable");
+                var oBinding = oList.getBinding("items");
+                    oBinding.filter(aFilters);
+            },
+
             onSelect: function(oEvent){
-                //Get the control list
-                var oList = oEvent.getSource();
-
-                //Get the selected item
-                // var oSelItem = oList.getSelectedItem();
-
-                //Get the context binding path
-                // var sSelItemPath = oSelItem.getBindingContextPath();
-                var sSelItemPath = oList.getBindingContextPath();
-
-                //Bind the selected item to the control
-                var oForm = this.getView().byId("headerForTest");
-
-                oForm.bindElement({
-                    path: sSelItemPath,
-                    model: "Employee"
-                })
-
+                var sEmployeeID = oEvent.getSource().getCells()[0].getText();
+                this.getRouter().navTo("RouteViewEmployee", {
+                    EmployeeID: sEmployeeID
+                });
             },
 
             onAdd: function() {
@@ -39,7 +48,46 @@ sap.ui.define([
             },
 
             onDelete: function() {
-                MessageBox.confirm("Are you sure you want to delete the selected employee/s?");
+                var oModel = this.getOwnerComponent().getModel("EmployeesList");
+
+                var oList = this.byId("idEmployeesTable");
+                var items = oList.getSelectedItems();
+
+                if (items.length > 0) {
+                    MessageBox.confirm("Are you sure you want to delete the selected employee/s?", {
+                        actions: [MessageBox.Action.YES, MessageBox.Action.CLOSE],
+                        emphasizedAction: MessageBox.Action.YES,
+                        onClose: function (sAction) {
+                            if (sAction === sap.m.MessageBox.Action.YES) {
+                                var arrFailed = [];
+
+                                for (var i = 0; i < items.length; i++) {
+                                    var item = items[i];
+
+                                    var sEmployeeID = item.getCells()[0].getText();
+
+                                    oModel.remove("/Employee(EmployeeID='"+ sEmployeeID +"')", {
+                                        success: function () {
+                                            
+                                        },
+                    
+                                        error: function () {
+                                            arrFailed.push(sEmployeeID);
+                                        }
+                                    });
+                                }
+
+                                if (arrFailed.length > 0) {
+                                    MessageToast.show("Failed to delete some employee/s.");
+                                } else {
+                                    MessageToast.show("Employee/s has been deleted.");
+                                }
+                            }
+                        }
+                    });
+                } else {
+                    MessageBox.warning("Please select employee/s.");
+                }
             },
 
             getRouter: function() {
